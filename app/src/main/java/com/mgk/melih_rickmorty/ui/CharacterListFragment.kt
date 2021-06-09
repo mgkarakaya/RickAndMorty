5af135d03060a0bfb6a06453690f030fad929169
mgkarakaya <mkarakaya6675@conestogac.on.ca>
2021-06-09 05:39:48 -0400

    Exposing list as a livedata so it survives outside the Fragment lifecycle.

--- app/src/main/java/com/mgk/melih_rickmorty/ui/CharacterListFragment.kt
+++ app/src/main/java/com/mgk/melih_rickmorty/ui/CharacterListFragment.kt
@@ -1,18 +1,13 @@
 package com.mgk.melih_rickmorty.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.appcompat.widget.SearchView
-import androidx.fragment.app.Fragment
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewModelScope
-import androidx.navigation.fragment.findNavController
-import androidx.paging.PagedList
-import androidx.paging.PagingData
-import androidx.paging.filter
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,11 +15,9 @@ import com.mgk.melih_rickmorty.R
 import com.mgk.melih_rickmorty.adapter.CharacterListAdapter
 import com.mgk.melih_rickmorty.autoFitColumns
 import com.mgk.melih_rickmorty.databinding.CharacterListFragmentBinding
-import com.mgk.melih_rickmorty.model.CharacterSingle
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+
 /*
 * Fragment that lists all characters from rickandmorty api.
 * has a toolbar includes Grid/List layout changer icon (chooseLayout)
@@ -34,56 +27,69 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class CharacterListFragment : Fragment() {
 
-    private lateinit var binding:CharacterListFragmentBinding
+    private lateinit var binding: CharacterListFragmentBinding
     private val characterListViewModel: CharacterListViewModel by viewModels()
     private lateinit var recyclerView: RecyclerView
     private var isLinearLayoutManager: Boolean = true
     private val adapter = CharacterListAdapter()
-    private var searchJob: Job? = null
-    private var searchKeyword:String ?=null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= CharacterListFragmentBinding.inflate(inflater,container,false)
+        binding = CharacterListFragmentBinding.inflate(inflater, container, false)
 
         setHasOptionsMenu(true)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        recyclerView=binding.recyclerView
+        recyclerView = binding.recyclerView
         chooseLayout()
-        recyclerView.adapter=adapter
-        search(searchKeyword)
+        recyclerView.adapter = adapter
+
+        characterListViewModel.search()
+        characterListViewModel.charactersPagingData.observe(viewLifecycleOwner) { pagingData ->
+            lifecycleScope.launch {
+                adapter.submitData(pagingData)
+            }
+        }
     }
 
-    private fun chooseLayout(){
-        if(isLinearLayoutManager){
-            recyclerView.layoutManager= LinearLayoutManager(context)
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(context)
             recyclerView.addItemDecoration(
-                DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
-        }
-        else{
+                DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+            )
+        } else {
             recyclerView.autoFitColumns(100)
             recyclerView.addItemDecoration(
-                DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL))
+                DividerItemDecoration(context, DividerItemDecoration.HORIZONTAL)
+            )
         }
 
     }
+
     private fun setIcon(menuItem: MenuItem?) {
         if (menuItem == null)
             return
         // Set the drawable for the menu icon based on which LayoutManager is currently in use
-        menuItem.icon =
-            when {
-                isLinearLayoutManager -> ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_grid_layout)
-                else -> ContextCompat.getDrawable(this.requireContext(), R.drawable.ic_linear_layout)
-            }
+        menuItem.icon = when {
+            isLinearLayoutManager -> ContextCompat.getDrawable(
+                this.requireContext(),
+                R.drawable.ic_grid_layout
+            )
+            else -> ContextCompat.getDrawable(
+                this.requireContext(),
+                R.drawable.ic_linear_layout
+            )
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.layout_menu,menu)
+        menuInflater.inflate(R.menu.layout_menu, menu)
         val layout_button = menu.findItem(R.id.action_switch_layout)
         setIcon(layout_button)
 
@@ -91,22 +97,28 @@ class CharacterListFragment : Fragment() {
 
         val searchView = search.actionView as SearchView
         searchView.queryHint = "Search"
+
+        //restore searchview after coming back from detail fragment
+        searchView.setQuery(characterListViewModel.searchKeyword ?: "", false)
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
+
             override fun onQueryTextChange(newText: String?): Boolean {
 
-                searchKeyword=newText
-                search(searchKeyword)
+                if (!isVisible) {
+                    return true
+                }
+                characterListViewModel.searchKeyword = newText
                 return true
             }
         })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId){
-            R.id.action_switch_layout->{
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
                 // Sets isLinearLayoutManager (a Boolean) to the opposite value
                 isLinearLayoutManager = !isLinearLayoutManager
                 // Sets layout and icon
@@ -117,18 +129,6 @@ class CharacterListFragment : Fragment() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-    private fun search(query: String?) {
 
-        // Make sure we cancel the previous job before creating a new one
-        searchJob?.cancel()
-        searchJob = lifecycleScope.launch {
-            characterListViewModel.getCharacters()
-                .collectLatest {
-                    adapter.submitData(it
-                        .filter { query?.let { it1 -> it.name.contains(it1,ignoreCase = true) } ?:true })
-                }
-
-        }
-    }
 
 }
--- app/src/main/java/com/mgk/melih_rickmorty/ui/CharacterListViewModel.kt
+++ app/src/main/java/com/mgk/melih_rickmorty/ui/CharacterListViewModel.kt
@@ -1,11 +1,17 @@
 package com.mgk.melih_rickmorty.ui
 
-import androidx.lifecycle.*
-import androidx.paging.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.filter
 import com.mgk.melih_rickmorty.data.RmRepository
 import com.mgk.melih_rickmorty.model.CharacterSingle
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,11 +23,26 @@ import javax.inject.Inject
 class CharacterListViewModel @Inject constructor(private val repository: RmRepository) :
     ViewModel() {
 
+    var searchKeyword: String? = null
+        set(value) {
+            field = value
+            search()
+        }
 
-    fun getCharacters(): Flow<PagingData<CharacterSingle>> {
-
-        return repository.getSearchResultStream().cachedIn(viewModelScope)
-
+    private val flow: Flow<PagingData<CharacterSingle>> by lazy {
+        repository.getSearchResultStream().cachedIn(viewModelScope)
     }
 
+    private var _charactersPagingData = MutableLiveData<PagingData<CharacterSingle>>()
+    val charactersPagingData: LiveData<PagingData<CharacterSingle>> = _charactersPagingData
+
+    fun search() = viewModelScope.launch {
+        flow.collectLatest {
+            _charactersPagingData.postValue(it.filter { character ->
+                searchKeyword?.let { text ->
+                    character.name.contains(text, ignoreCase = true)
+                } ?: true
+            })
+        }
+    }
 }
